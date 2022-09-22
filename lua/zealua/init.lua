3d0fdb6a573f1f8b5320a982d984d3f376e5a4fb
Thomas TEIXEIRA <avanalos@gmail.com>
2022-09-22 21:54:20 +0200

    feat: completion function for ZealFT

--- lua/zealua/init.lua
+++ lua/zealua/init.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.options = {
 	location = "$HOME/.local/share/Zeal/Zeal/docsets",
+	completion_cmd = "find %s -maxdepth 4 -name Info.plist -exec grep -o -m 1 -E '<string>[a-z]*</string>' {} \\; | uniq",
 	docsets = {},
 	buffer_filetype = {},
 }
@@ -15,7 +16,7 @@ M.setup = function(user_config)
 
 	-- user commands
 	vim.api.nvim_create_user_command('Zeal', M.search, { nargs = '?', bang = true })
-	-- vim.api.nvim_create_user_command('ZealEngine', M.engine, {})
+	vim.api.nvim_create_user_command('ZealEngine', M.engine, {})
 	vim.api.nvim_create_user_command('ZealFT', M.filetype, { nargs = 1, bar = true, complete = M._completion })
 
 	-- keymaps
@@ -27,13 +28,8 @@ end
 --====================================================
 --==========              Zeal              ==========
 --====================================================
-M.engine = function()
-	-- TODO Asks for both docset and search word (if non -> use <cword>)
-	-- 		Uses the M.completion for the first argument only (might need some research)
-end
 
 M.search = function(cmd)
-	local bufnr = vim.fn.bufnr()
 	local filetype = M._get_filetype()
 	local word = cmd.args ~= '' and cmd.fargs[1] or vim.fn.expand('<cword>')
 
@@ -45,6 +41,11 @@ M.search = function(cmd)
 	end
 
 	M._launch(search)
+end
+
+M.engine = function(cmd)
+	-- TODO Asks for both docset and search word (if non -> use <cword>)
+	-- 		Uses the M.completion for the first argument only (might need some research)
 end
 
 -- Use to correct incoherent buffer filetype
@@ -68,12 +69,30 @@ M._get_filetype = function()
 end
 
 M._completion = function(_, _, _)
-	-- lazy loading docsets completion
-	if not M.options.docsets then
-		print("HELLO")
+	-- lazy loading docsets completion when they're first needed
+	if #M.options.docsets == 0 then
+		M._fill_completion()
 	end
 
 	return M.options.docsets
+end
+
+M._fill_completion = function()
+	local command = string.format(M.options.completion_cmd, M.options.location)
+	local results = io.popen(command, "r")
+	if not results then
+		return
+	end
+
+	for line in results:lines() do
+		-- since all lines follow the same pattern
+		-- <script>the_dash_name</script>
+		-- we can just grab the middle value with a simple
+		-- string.sub call.
+		local docset = string.sub(line, 9, -10)
+		table.insert(M.options.docsets, docset)
+	end
+	results:close()
 end
 
 M._clear_hit_enter = function()
